f0190a3d9c2eb93b37981f88af06262049e81bce
Koushi19 <mohankoushik.nalam@infodatinc.com>
2024-07-27 11:56:57 -0500

    temp changes

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -16,7 +16,7 @@ const renderContent = {
   1: (
     <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
       Hi, I am<span className="font-semibold"> Koushik 👋</span>
-      <br />A FullStack Developer
+      <br />A FullStack Software Developer
     </h1>
   ),
   2: (
